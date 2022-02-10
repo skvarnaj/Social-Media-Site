c20028b46e9b269811cc593469a59090f976aa25
Joe Skvarna <jnskvarna@ucdavis.edu>
2022-02-09 18:03:18 -0800

    added reset password through an email sent to user

--- App/__init__.py
+++ App/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '447cc3d7257853eafabc30fd9c373ff8'
@@ -11,5 +13,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'noreplyb77988443@gmail.com'
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+#os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
+print(app.config['MAIL_PASSWORD'])
 from App import routes
